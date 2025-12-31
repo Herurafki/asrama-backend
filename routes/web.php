227b1b09fae9c6e00961c58eaa9800e092745120
Herurafki <herurafki11@gmail.com>
2025-12-31 17:59:26 +0700

    change default route to admin filament

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/login');
 });
 
 Route::get('/sanctum/csrf-cookie', function () {
